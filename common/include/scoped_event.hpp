a22c0594eb6aec47f9ea0e54005fc665501fb231
akrivx <cakriv@gmail.com>
2025-11-14 00:56:49 +0000

    Refactor scoped event and timer

--- common/include/scoped_event.hpp
+++ common/include/scoped_event.hpp
@@ -1,30 +1,52 @@
 #pragma once
 
+#include <memory>
 #include <cuda_runtime.h>
-#include "noncopyable.hpp"
 #include "cuda_check.hpp"
 
 
 namespace gpu_lab {
-  
-  struct ScopedEvent : NonCopyable {
-    explicit ScopedEvent(unsigned int flags = cudaEventDefault) {
-      CUDA_CHECK(cudaEventCreateWithFlags(&e_, flags));
-    }
 
-    ~ScopedEvent() {
-      CUDA_CHECK(cudaEventDestroy(e_));
+  namespace detail {
+    struct EventDeleter {
+      using pointer = cudaEvent_t;
+      void operator()(cudaEvent_t e) const noexcept {
+        cudaEventDestroy(e);
+      }
+    };
+
+    using UniqueEvent = std::unique_ptr<cudaEvent_t, detail::EventDeleter>;
+
+    inline auto make_unique_event(unsigned int flags) {
+      cudaEvent_t e{};
+      CUDA_CHECK(cudaEventCreateWithFlags(&e, flags));
+      return UniqueEvent{e};
     }
+  }
+
+  struct ScopedEvent {
+    explicit ScopedEvent(unsigned int flags = cudaEventDefault)
+      : ev_{detail::make_unique_event(flags)} {}
 
     void record(cudaStream_t stream = cudaStreamDefault) const {
-      CUDA_CHECK(cudaEventRecord(e_, stream));
+      CUDA_CHECK(cudaEventRecord(ev_.get(), stream));
     }
 
     void sync() const {
-      CUDA_CHECK(cudaEventSynchronize(e_));
+      CUDA_CHECK(cudaEventSynchronize(ev_.get()));
     }
 
-    cudaEvent_t e_;
+    float elapsed_time_from(const ScopedEvent& start) const {
+      float ms = {};
+      CUDA_CHECK(cudaEventElapsedTime(&ms, start.ev_.get(), ev_.get()));
+      return ms;
+    }
+
+    cudaEvent_t get() const {
+      return ev_.get();
+    }
+
+    detail::UniqueEvent ev_;
   };
 
 }
---
+++ common/include/with_timer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include "scoped_event.hpp"
+
+
+namespace gpu_lab {
+
+  template<typename F>
+  auto with_timer(F&& f, cudaStream_t stream = cudaStreamDefault) {
+    ScopedEvent start;
+    ScopedEvent stop;
+    start.record(stream);
+    f();
+    stop.record(stream);
+    stop.sync();
+    return stop.elapsed_time_from(start);
+  }
+
+}
