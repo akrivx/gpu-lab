f9227c8380dd06c261920c685b4acd3792e216a2
akrivx <cakriv@gmail.com>
2025-12-06 17:17:01 +0000

    Use `std::size_t` in image

--- common/include/image.hpp
+++ common/include/image.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <cstddef>
 #include <memory>
 #include <utility>
 
@@ -26,7 +27,7 @@ namespace gpu_lab {
     Image(const Image&) = delete;
     Image& operator=(const Image&) = delete;
 
-    Image(size_t w, size_t h)
+    Image(std::size_t w, std::size_t h)
       : width_{w}
       , height_{h}
     {
@@ -61,10 +62,10 @@ namespace gpu_lab {
     const element_type* data() const noexcept { return handle_.get(); }
     const element_type* cdata() const noexcept { return handle_.get(); }
 
-    size_t width() const noexcept { return width_; }
-    size_t height() const noexcept { return height_; }
-    size_t pitch_bytes() const noexcept { return pitch_bytes_; }
-    size_t size_bytes() const noexcept { return height_ * pitch_bytes_; }
+    std::size_t width() const noexcept { return width_; }
+    std::size_t height() const noexcept { return height_; }
+    std::size_t pitch_bytes() const noexcept { return pitch_bytes_; }
+    std::size_t size_bytes() const noexcept { return height_ * pitch_bytes_; }
     bool empty() const noexcept { return handle_ == nullptr; }
 
     view_type view() noexcept { return image_view(data(), width_, height_, pitch_bytes_); }
@@ -72,9 +73,9 @@ namespace gpu_lab {
     const_view_type cview() const noexcept { return image_view(data(), width_, height_, pitch_bytes_); }
     
   private:
-    size_t width_       = {};
-    size_t height_      = {};
-    size_t pitch_bytes_ = {};
-    handle_type handle_ = {};
+    std::size_t width_       = {};
+    std::size_t height_      = {};
+    std::size_t pitch_bytes_ = {};
+    handle_type handle_      = {};
   };
 }
