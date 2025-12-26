e37ae3f1785280c9c2cb0b55f3ed41b33b8ed07c
akrivx <cakriv@gmail.com>
2025-12-26 17:28:34 +0000

    Improve CUDA error checking
    - Improve error messages.
    - Introduce non-throwing check for cases like checking from within noexcept functions and destructors.

--- common/include/cuda_check.hpp
+++ common/include/cuda_check.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdio>
+#include <exception>
+#include <format>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -7,22 +10,63 @@
 #include <cuda_runtime.h>
 
 namespace gpu_lab {
+  namespace detail {
+    struct CudaErrorInfo {
+      cudaError_t err;
+      const char* expr;
+      const char* file;
+      int line;
+
+      void write(FILE* fp) const noexcept {
+        std::fprintf(fp,
+                     "FATAL CUDA ERROR: %s: (%s)\n  at %s:%d\n  expr: %s\n",
+                     cudaGetErrorName(err),
+                     cudaGetErrorString(err),
+                     file,
+                     line,
+                     expr);
+      }
+
+      std::string to_string() const {
+        return std::format("{} ({})\n  at {}:{}\n  expr: {}\n",
+                           cudaGetErrorName(err),
+                           cudaGetErrorString(err),
+                           file,
+                           line,
+                           expr);
+      }
+    };
+
+    [[noreturn]] inline void cuda_terminate(CudaErrorInfo e) noexcept {
+      e.write(stderr);
+      std::fflush(stderr);
+      std::terminate();
+    }
+  } // namespace detail
+
   class CudaError : public std::runtime_error {
   public:
-    CudaError(cudaError_t err, std::string_view msg)
-        : std::runtime_error{std::string(cudaGetErrorName(err)) + ": " + cudaGetErrorString(err)
-                             + " | " + std::string{msg}} {}
+    CudaError(detail::CudaErrorInfo e)
+        : std::runtime_error{e.to_string()} {}
   };
 
   namespace detail {
-    inline void cuda_check_impl(cudaError_t err, std::string_view msg) {
+    inline void
+    cuda_check_terminate(cudaError_t err, const char* expr, const char* file, int line) noexcept {
       if (err != cudaSuccess) {
-        throw CudaError{err, msg};
+        cuda_terminate({err, expr, file, line});
+      }
+    }
+
+    inline void cuda_check_throw(cudaError_t err, const char* expr, const char* file, int line) {
+      if (err != cudaSuccess) {
+        throw CudaError{CudaErrorInfo{err, expr, file, line}};
       }
     }
   } // namespace detail
 } // namespace gpu_lab
 
-#define CUDA_CHECK(expr)                                                                           \
-  gpu_lab::detail::cuda_check_impl((expr),                                                         \
-                                   std::string_view{__FILE__ ":" + std::to_string(__LINE__)})
+#define CUDA_CHECK(expr) gpu_lab::detail::cuda_check_throw((expr), #expr, __FILE__, __LINE__)
+
+#define CUDA_CHECK_TERMINATE(expr)                                                                 \
+  gpu_lab::detail::cuda_check_terminate((expr), #expr, __FILE__, __LINE__)
