94a31dd49c5035567fcb200d04c28cdfe56c864f
akrivx <cakriv@gmail.com>
2026-01-09 10:20:41 +0000

    Require trivially desstructible pitched elements

--- common/include/pitched_element.hpp
+++ common/include/pitched_element.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
-#include <cstddef>
 #include <type_traits>
 
 #include "gpu_lab_config.hpp" // generated in the build dir
 
 namespace gpu_lab {
   template <class T>
-  concept PitchedElement =
-      std::is_trivially_copyable_v<T> && (cuda_pitch_alignment % sizeof(T) == 0);
+  concept PitchedElement = std::is_trivially_destructible_v<T> && std::is_trivially_copyable_v<T>
+                           && (cuda_pitch_alignment % sizeof(T) == 0);
 } // namespace gpu_lab
