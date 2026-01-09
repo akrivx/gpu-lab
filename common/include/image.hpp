fd84b790562db7f0c6bdc1ec57f79a11ee692b45
akrivx <cakriv@gmail.com>
2026-01-09 10:45:37 +0000

    Refactor image using strided byte storage

--- common/include/image.hpp
+++ common/include/image.hpp
@@ -7,8 +7,10 @@
 
 #include <cuda_runtime.h>
 
+#include "byte_storage.hpp"
 #include "image_view.hpp"
 #include "memory_location.hpp"
+#include "memory_resource.hpp"
 #include "pitched_element.hpp"
 #include "tiled_image_view.hpp"
 #include "unique_array.hpp"
@@ -16,6 +18,8 @@
 namespace gpu_lab {
   template <PitchedElement T, MemoryLocation Loc>
   class [[nodiscard]] Image {
+    using resource_type = detail::DefaultMemoryResource<Loc>;
+
   public:
     using handle_type = UniqueArray<T, Loc>;
     using element_type = T;
@@ -30,62 +34,55 @@ namespace gpu_lab {
     Image& operator=(const Image&) = delete;
 
     Image(std::size_t h, std::size_t w)
-        : height_{h}
-        , width_{w} {
-      auto [handle, pitch_bytes] = make_unique_array2d<T, Loc>(h, w);
-      assert((pitch_bytes % sizeof(element_type)) == 0); // PitchedElement guarantees that
-      handle_ = std::move(handle);
-      pitch_ = pitch_bytes / sizeof(element_type);
+        : storage_{w * sizeof(T), h, alignof(T)}
+        , width_{w}
+        , pitch_{storage_.allocation().stride_bytes / sizeof(T)} {
+      assert((pitch_bytes() % sizeof(T)) == 0); // PitchedElement guarantees that
+      assert((width_bytes() / sizeof(T)) == w);
     }
 
     Image(Image&& o) noexcept
-        : height_{std::exchange(o.height_, {})}
-        , width_{std::exchange(o.width_, {})}
-        , pitch_{std::exchange(o.pitch_, {})}
-        , handle_{std::exchange(o.handle_, {})} {}
+        : storage_{std::exchange(o.storage_, {})}
+        , width_{std::exchange(o.width_, 0)}
+        , pitch_{std::exchange(o.pitch_, 0)} {}
 
     Image& operator=(Image&& o) noexcept {
       if (this != &o) {
-        height_ = std::exchange(o.height_, {});
-        width_ = std::exchange(o.width_, {});
-        pitch_ = std::exchange(o.pitch_, {});
-        handle_ = std::exchange(o.handle_, {});
+        storage_ = std::exchange(o.storage_, {});
+        width_ = std::exchange(o.width_, 0);
+        pitch_ = std::exchange(o.pitch_, 0);
       }
       return *this;
     }
 
-    handle_type release() noexcept {
-      height_ = {};
-      width_ = {};
-      pitch_ = {};
-      return std::exchange(handle_, {});
-    }
+    element_type* data() noexcept { return static_cast<T*>(storage_.allocation().ptr); }
+    const element_type* data() const noexcept { return static_cast<const T*>(storage_.allocation().ptr); }
+    const element_type* cdata() const noexcept { return static_cast<const T*>(storage_.allocation().ptr); }
 
-    element_type* data() noexcept { return handle_.get(); }
-    const element_type* data() const noexcept { return handle_.get(); }
-    const element_type* cdata() const noexcept { return handle_.get(); }
-
-    std::size_t height() const noexcept { return height_; }
     std::size_t width() const noexcept { return width_; }
+    std::size_t height() const noexcept { return storage_.allocation().block_count; }
     std::size_t pitch() const noexcept { return pitch_; }
-    std::size_t width_bytes() const noexcept { return width_ * sizeof(element_type); }
-    std::size_t pitch_bytes() const noexcept { return pitch_ * sizeof(element_type); }
-    std::size_t size_bytes() const noexcept { return height_ * pitch_bytes(); }
-    bool empty() const noexcept { return handle_ == nullptr; }
 
-    view_type view() noexcept { return image_view<Loc>(data(), pitch_, height_, width_); }
+    std::size_t width_bytes() const noexcept { return storage_.allocation().block_bytes; }
+    std::size_t pitch_bytes() const noexcept { return storage_.allocation().stride_bytes; }
+    std::size_t size_bytes() const noexcept { return storage_.allocation().total_bytes(); }
+
+    bool empty() const noexcept { return size_bytes() == 0; }
+
+    view_type view() noexcept { return image_view<Loc>(data(), pitch(), height(), width()); }
+
     const_view_type view() const noexcept {
-      return image_view<Loc>(data(), pitch_, height_, width_);
+      return image_view<Loc>(data(), pitch(), height(), width());
     }
+
     const_view_type cview() const noexcept {
-      return image_view<Loc>(data(), pitch_, height_, width_);
+      return image_view<Loc>(data(), pitch(), height(), width());
     }
 
   private:
-    std::size_t height_ = {};
+    detail::StridedByteStorage<resource_type> storage_ = {};
     std::size_t width_ = {};
-    std::size_t pitch_ = {}; // in elements
-    handle_type handle_ = {};
+    std::size_t pitch_ = {};
   };
 
   namespace detail {
